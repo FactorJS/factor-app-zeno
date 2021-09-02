72ff1e6f1e06b82a779601c33eea587992e290aa
Andrew Powers <arpowers@gmail.com>
2021-09-02 13:38:21 -0700

    feat: init

---
+++ src/el/figure-devops.vue
@@ -0,0 +1,13 @@
+<template>
+  <figure class="bg-center bg-no-repeat">
+    <img src="../img/devops.svg" alt="devops" class="relative z-30 h-full w-full object-contain" />
+  </figure>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
